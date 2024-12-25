79bf9754e13f7d609a1389ea6620c7aa62739e76
Victor <victor@localhost.localdomain>
2024-12-25 23:10:28 +0100

    dup

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 10:31:37 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/06 23:43:16 by victor           ###   ########.fr       */
+/*   Updated: 2024/12/25 23:07:15 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	i;
 
+	if (!dst || !src)
+		return (0);
 	src_len = ft_strlen(src);
 	i = 0;
 	if (size > 0)
